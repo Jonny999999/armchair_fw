37e9048daac1bc50ded3d735ad19d344f2eb64df
jonny_ji7 <jonny@wwad.de>
2022-06-03 10:03:25 +0200

    Changes for C++ support, Add gitigore
    - extern C around libraries and main function
    - Include common libraries
    - Create gitignore

---
+++ main/main.cpp
@@ -0,0 +1,18 @@
+extern "C"
+{
+#include <stdio.h>
+#include <esp_system.h>
+#include <esp_event.h>
+#include <nvs_flash.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+#include "esp_log.h"
+#include "sdkconfig.h"
+}
+
+extern "C" void app_main(void)
+{
+
+
+}
