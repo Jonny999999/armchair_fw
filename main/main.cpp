b0d0b568a314eb13df2b37cc0784c07f4f072725
jonny_ji7 <jonny@wwad.de>
2022-06-04 15:57:29 +0200

    Create driver for 'single100a h-bridge'
    - create class 'single100a' in motordrivers.hpp and motordrivers.cpp
    - add code for testing the driver in main.cpp (test with led was successful)
    - update cmakelists, to also compile the new .cpp file

--- main/main.cpp
+++ main/main.cpp
@@ -9,10 +9,54 @@ extern "C"
 #include "driver/gpio.h"
 #include "esp_log.h"
 #include "sdkconfig.h"
+
+#include "driver/ledc.h"
+
 }
 
-extern "C" void app_main(void)
-{
+#include "motordrivers.hpp"
 
 
+extern "C" void app_main(void) {
+
+    //set loglevel for all tags:
+    esp_log_level_set("*", ESP_LOG_INFO);
+    //set loglevel for motordriver to DEBUG for testing
+    esp_log_level_set("motordriver", ESP_LOG_DEBUG);
+
+    //configure motor driver
+    single100a_config_t configDriverLeft = {
+        .gpio_pwm = GPIO_NUM_14,
+        .gpio_a = GPIO_NUM_12,
+        .gpio_b = GPIO_NUM_13,
+        .ledc_timer = LEDC_TIMER_0,
+        .ledc_channel = LEDC_CHANNEL_0,
+        .abInverted = true,
+        .resolution = LEDC_TIMER_11_BIT,
+        .pwmFreq = 10000
+    };
+
+    //create driver instance for motor left
+    single100a motorLeft(configDriverLeft);
+
+
+    while(1){
+
+        //--- testing the motor driver ---
+        //fade up duty - forward
+        for (int duty=0; duty<=100; duty+=5) {
+            motorLeft.set(motorstate_t::FWD, duty);
+            vTaskDelay(100 / portTICK_PERIOD_MS);
+        }
+        //brake for 1 s
+        motorLeft.set(motorstate_t::BRAKE);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        //stay at 100% - reverse
+        motorLeft.set(motorstate_t::REV, 150);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        //stay at idle (default duty)
+        motorLeft.set(motorstate_t::IDLE);
+        vTaskDelay(2000 / portTICK_PERIOD_MS);
+
+    }
 }
---
+++ main/motordrivers.hpp
@@ -0,0 +1,64 @@
+#pragma once
+
+extern "C"
+{
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+#include "esp_log.h"
+
+#include "driver/ledc.h"
+#include "esp_err.h"
+}
+
+#include <cmath>
+
+
+//====================================
+//===== single100a motor driver ======
+//====================================
+
+//--------------------------------------------
+//---- struct, enum, variable deklarations ---
+//--------------------------------------------
+
+//class which controls a motor using a 'single100a' h-bridge module
+enum class motorstate_t {IDLE, FWD, REV, BRAKE};
+//definition of string array to be able to convert state enum to readable string (defined in motordrivers.cpp)
+extern const char* motorstateStr[4];
+
+//struct with all config parameters for single100a motor driver
+typedef struct single100a_config_t {
+    gpio_num_t gpio_pwm;
+    gpio_num_t gpio_a;
+    gpio_num_t gpio_b;
+    ledc_timer_t ledc_timer;
+    ledc_channel_t ledc_channel;
+    bool abInverted;
+    ledc_timer_bit_t resolution;
+    int pwmFreq;
+} single100a_config_t;
+
+
+
+//--------------------------------
+//------- single100a class -------
+//--------------------------------
+class single100a {
+    public:
+        //--- constructor ---
+        single100a(single100a_config_t config_f); //provide config struct (see above)
+
+        //--- functions ---
+        void set(motorstate_t state, float duty_f = 0); //set mode and duty of the motor (see motorstate_t above)
+        //TODO: add functions to get the current state and duty
+
+    private:
+        //--- functions ---
+        void init(); //initialize pwm and gpio outputs, calculate maxDuty
+
+        //--- variables ---
+        single100a_config_t config;
+        uint32_t dutyMax;
+        motorstate_t state = motorstate_t::IDLE;
+};
