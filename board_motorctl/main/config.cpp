0a1f941de05d431ce5fb81dc05af88a352384dfb
jonny_jr9 <jonny@wwad.de>
2023-09-04 08:43:20 +0200

    Update configs according to new pin assignment
    current config of each board now matches pin assignment

--- board_motorctl/main/config.cpp
+++ board_motorctl/main/config.cpp
@@ -6,8 +6,8 @@
 //--- configure left motor (hardware) ---
 single100a_config_t configDriverLeft = {
     .gpio_pwm = GPIO_NUM_26,
-    .gpio_a = GPIO_NUM_16,
-    .gpio_b = GPIO_NUM_4,
+    .gpio_a = GPIO_NUM_4,
+    .gpio_b = GPIO_NUM_16,
     .ledc_timer = LEDC_TIMER_0,
     .ledc_channel = LEDC_CHANNEL_0,
 	.aEnabledPinState = false, //-> pins inverted (mosfets)
@@ -20,7 +20,7 @@ single100a_config_t configDriverLeft = {
 single100a_config_t configDriverRight = {
     .gpio_pwm = GPIO_NUM_27,
     .gpio_a = GPIO_NUM_2,
-    .gpio_b = GPIO_NUM_14,
+    .gpio_b = GPIO_NUM_15,
     .ledc_timer = LEDC_TIMER_1,
     .ledc_channel = LEDC_CHANNEL_1,
 	.aEnabledPinState = false, //-> pin inverted (mosfet)
@@ -35,8 +35,13 @@ single100a_config_t configDriverRight = {
 motorctl_config_t configMotorControlLeft = {
     .msFadeAccel = 1900, //acceleration of the motor (ms it takes from 0% to 100%)
     .msFadeDecel = 1000, //deceleration of the motor (ms it takes from 100% to 0%)
+<<<<<<< Updated upstream
 	.currentLimitEnabled = true,
+	.currentSensor_adc =  ADC1_CHANNEL_0, //GPIO36
+=======
+	.currentLimitEnabled = false,
 	.currentSensor_adc =  ADC1_CHANNEL_6, //GPIO34
+>>>>>>> Stashed changes
 	.currentSensor_ratedCurrent = 50,
     .currentMax = 30,
 	.deadTimeMs = 900 //minimum time motor is off between direction change
@@ -46,8 +51,13 @@ motorctl_config_t configMotorControlLeft = {
 motorctl_config_t configMotorControlRight = {
     .msFadeAccel = 1900, //acceleration of the motor (ms it takes from 0% to 100%)
     .msFadeDecel = 1000, //deceleration of the motor (ms it takes from 100% to 0%)
+<<<<<<< Updated upstream
 	.currentLimitEnabled = true,
+	.currentSensor_adc =  ADC1_CHANNEL_3, //GPIO39
+=======
+	.currentLimitEnabled = false,
 	.currentSensor_adc =  ADC1_CHANNEL_4, //GPIO32
+>>>>>>> Stashed changes
 	.currentSensor_ratedCurrent = 50,
     .currentMax = 30,
 	.deadTimeMs = 900 //minimum time motor is off between direction change
